17c97847da8ca97b69c4943161b040c19d2c44e3
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-09-25 22:39:20 +0200

    feat(3-functions): feat(3-functions): 3.3 Bean counting with 'countChar'

--- 3-functions/3.3-Bean-counting.js
+++ 3-functions/3.3-Bean-counting.js
@@ -1,14 +1,20 @@
-function countBs(string) {
+function countChar(stringToSearch, charToBeCounted) {
     let count = 0;
-    for (let index = 0; index < string.length; index++) {
-        if (string[index] === 'B') count++;
+    for (let index = 0; index < stringToSearch.length; index++) {
+        if (stringToSearch[index] === charToBeCounted) count++;
     }
     return count;
+
+}
+
+function countBs(stringToSearch) {
+    return countChar(stringToSearch, "B");
 }
 
 function test() {
-    console.assert(countBs("BOB") === 2, "❌");
-    console.assert(countBs("bob") === 0, "❌");
+    console.assert(countBs("BOB") === 2, "❌ BOB");
+    console.assert(countBs("bob") === 0, "❌ bob");
+    console.assert(countChar("kakkerlak", "k") === 4, "❌ kakkerlak");
 }
 
 test();
